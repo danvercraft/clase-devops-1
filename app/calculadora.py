880d46c4395bc1af1003ec6afa842e8192d15496
Danvercraft <san290200@gmail.com>
2025-04-05 18:45:33 -0500

    UPDATING

--- app/calculadora.py
+++ app/calculadora.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     resultado = sumar(x, y)
-    print(f"Resultado: {resultado}")
+    print(f"Resultado es: {resultado}")
